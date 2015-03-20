c16e58d91d20a88d7b9626e0478a174dfd5f21a4
Aleksey Uimanov <s9gf4ult@gmail.com>
2015-03-20 23:50:25 +0500

    compilable

--- src/Database/Redis/Config.hs
+++ src/Database/Redis/Config.hs
@@ -2,13 +2,33 @@ module Database.Redis.Config where
 
 import Control.Applicative
 import Data.Aeson
+import Data.Scientific
+import Data.Time
 import Database.Redis
 
+import qualified Data.Aeson.Types as A
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
 
+
+-- | Wrapper around 'ConnectInfo' to parse connection settings from
+-- JSON-like values.
 newtype RedisConfig =
     RedisConfig
-    { rcConnectInfo :: ConnectInfo
+    { getConnectInfo :: ConnectInfo
     }
+
+newtype RedisPort =
+    RedisPort
+    { getPortID :: PortID
+    }
+
+instance FromJSON RedisPort where
+    parseJSON (A.String t) =
+        return $ RedisPort $ UnixSocket $ T.unpack t
+    parseJSON (A.Number n) =
+        return $ RedisPort $ PortNumber $ floor n
+    parseJSON _ = fail "RedisPort must be either string or number"
 
 instance FromJSON RedisConfig where
     parseJSON v = RedisConfig <$> withObject "RedisConfig" go v
@@ -16,8 +36,8 @@ instance FromJSON RedisConfig where
         go o =
             ConnInfo
             <$> (o .:? "host" .!= (connectHost defaultConnectInfo ))
-            <*> (o .:? "port" .!= (connectPort defaultConnectInfo ))
-            <*> (o .:? "password" .!= (connectAuth defaultConnectInfo))
+            <*> (fmap (fmap getPortID) (o .:? "port") .!= (connectPort defaultConnectInfo ))
+            <*> (fmap (fmap T.encodeUtf8) (o .:? "password"))
             <*> (o .:? "database" .!= (connectDatabase defaultConnectInfo))
             <*> (o .:? "max-connections" .!= (connectMaxConnections defaultConnectInfo))
-            <*> (o .:? "max-idle-time" .!= (connectMaxIdleTime defaultConnectInfo))
+            <*> (fmap (fmap (realToFrac :: Scientific -> NominalDiffTime)) (o .:? "max-idle-time") .!= (connectMaxIdleTime defaultConnectInfo))
